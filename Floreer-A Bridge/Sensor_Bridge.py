f3ef419e387a18900b6470a4bf707be17e18b39b
TimothyBubble <156446199+TimothyBubble@users.noreply.github.com>
2026-09-07 14:58:45 +0200

    Added comments and placeholder values to Sensor_Bridge.py
    - Commented on hardcoded fields
    - Commented what would be a good option te fix it to get rid of the technical debt

--- Floreer-A Bridge/Sensor_Bridge.py
+++ Floreer-A Bridge/Sensor_Bridge.py
@@ -2,14 +2,16 @@ import serial
 import requests
 import time
 
-SERIAL_PORT = "COM3"
+# TO-DO: Create SENSOR_ID automatically through env_id because ID's are not always the same
+# Low priority right now because of low number of sensors
+SERIAL_PORT = "/dev/ttyACM0" # Linux/Pi serial port or "COMx" on Windows
 BAUD_RATE = 9600
-API_URL = "http://localhost:8080/api/readings"
+API_URL = "http://<your-ip>:8080/api/readings" # Input your machine's actual local IP
 SENSOR_ID = 4
 PLANT_ID = 1
 
 ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=2)
-time.sleep(2)  # give the Arduino a moment to reset after opening the port
+time.sleep(2)  # gives the Arduino a moment to reset after opening the port
 
 while True:
     line = ser.readline().decode("utf-8").strip()
@@ -17,6 +19,8 @@ while True:
         value = float(line)
         payload = {
             "value": value,
+            # Uses UTC time instead of local to keep timestamps consistent
+            # time.strftime for easier to read time/date
             "timeStamp": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime()),
             "sensor": {"id": SENSOR_ID},
             "plant": {"id": PLANT_ID}
